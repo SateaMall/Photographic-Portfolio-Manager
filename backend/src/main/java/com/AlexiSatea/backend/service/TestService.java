245d97f4ae1b59d0d20f3beab0d85873a8ccb7eb
SateaMall <75503482+SateaMall@users.noreply.github.com>
2026-03-19 13:13:01 +0100

    working in postman tests and writing them

--- backend/src/main/java/com/AlexiSatea/backend/service/TestService.java
+++ backend/src/main/java/com/AlexiSatea/backend/service/TestService.java
@@ -1,5 +1,6 @@
 package com.AlexiSatea.backend.service;
 
+import com.AlexiSatea.backend.model.profile.Profile;
 import com.AlexiSatea.backend.model.user.AppUser;
 import com.AlexiSatea.backend.model.user.UserRole;
 import com.AlexiSatea.backend.repo.AppUserRepository;
@@ -17,12 +18,20 @@ public class TestService {
     @Autowired
     private PasswordEncoder passwordEncoder;
     public String signUpTest(){
+        Profile profile = Profile.builder()
+                .slug("Satea")
+                .displayName("Mohamad Satea ALMALLOUHI")
+                .bio("Amature photographer, interesed in photography")
+                .publicEmail("sate3.mallouhi@gmail.com")
+                .linkedIn("")
+                .build();
         AppUser appUser = AppUser.builder()
                 .email("sate3.mallouhi@gmail.com")
                 .firstName("sate3")
                 .lastName("mallouhi")
                 .passwordHash(passwordEncoder.encode("00000000"))
                 .role(UserRole.ADMIN)
+
                 .build();
         userRepository.save(appUser);
         return appUser.toString();
