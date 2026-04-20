a50da579a26c96b3060aa8539b766f1b64fb14cc
SateaMall <75503482+SateaMall@users.noreply.github.com>
2026-04-20 13:13:27 +0200

    committing bugfixes and naming incompatibility

--- backend/src/test/java/com/letmelens/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/letmelens/backend/BackendApplicationTests.java
@@ -1,10 +1,24 @@
 package com.letmelens.backend;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class BackendApplicationTests {
+
+	@TestConfiguration
+	static class TestConfig {
+		@Bean
+		JavaMailSender javaMailSender() {
+			return Mockito.mock(JavaMailSender.class);
+		}
+	}
 
 	@Test
 	void contextLoads() {
